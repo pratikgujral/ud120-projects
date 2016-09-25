79f38a8e87d7058760d4a9e5eb8a4cc811099000
Pratik Gujral <Pratik Gujral>
2016-09-25 10:21:17 +0530

    Removed all code except C=1000 and this time for complete dataset

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,74 +23,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn.svm import SVC
-
-clf = SVC(kernel='rbf') # By-default, C = 1.0
-print "C = 1.0"
 from time import time
-t0 = time()
 
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
-
-clf.fit(features_train, labels_train)
-print "Training Time SVM:", round(time()-t0,3), "seconds"
-
-t1 = time()
-pred = clf.predict(features_test)
-print "Prediction Time SVM:", round(time()-t1,3), "seconds"
-
-from sklearn.metrics import accuracy_score
-print accuracy_score(pred, labels_test)
-#########################################################
-
- # C = 10
-clf = SVC(kernel='rbf', C = 10.)
-print "C = 10.0"
-
-t0 = time()
-
-clf.fit(features_train, labels_train)
-print "Training Time SVM:", round(time()-t0,3), "seconds"
-
-t1 = time()
-pred = clf.predict(features_test)
-print "Prediction Time SVM:", round(time()-t1,3), "seconds"
-
-from sklearn.metrics import accuracy_score
-print accuracy_score(pred, labels_test)
-#########################################################
- # C = 100
-clf = SVC(kernel='rbf', C = 100.)
-print "C = 100.0"
-
-t0 = time()
-
-clf.fit(features_train, labels_train)
-print "Training Time SVM:", round(time()-t0,3), "seconds"
-
-t1 = time()
-pred = clf.predict(features_test)
-print "Prediction Time SVM:", round(time()-t1,3), "seconds"
-
-from sklearn.metrics import accuracy_score
-print accuracy_score(pred, labels_test)
-#########################################################
- # C = 1000
-clf = SVC(kernel='rbf', C = 1000.)
-print "C = 1000.0"
-
-t0 = time()
-
-clf.fit(features_train, labels_train)
-print "Training Time SVM:", round(time()-t0,3), "seconds"
-
-t1 = time()
-pred = clf.predict(features_test)
-print "Prediction Time SVM:", round(time()-t1,3), "seconds"
-
-from sklearn.metrics import accuracy_score
-print accuracy_score(pred, labels_test)
-#########################################################
  # C = 10000
 clf = SVC(kernel='rbf', C = 10000.)
 print "C = 10000.0"
